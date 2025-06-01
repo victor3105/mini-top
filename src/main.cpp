3755ae8a5e38ca232c8a1683ddad406ed8c8777e
Viktor Glebov <viktor.glebov94@gmail.com>
2025-06-01 17:56:08 +0400

    Calculate total and per-process stats concurrently

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <future>
 #include <iostream>
 
 #include "ProcessTable.h"
@@ -11,27 +12,40 @@ int main() {
   ProcessTable procTable;
 
   while (1) {
+    auto memFuture =
+        std::async(std::launch::async, [&]() { return info.getMemoryUsage(); });
+
+    auto cpuFuture =
+        std::async(std::launch::async, [&]() { return info.getCpuUsage(); });
+
+    auto procFuture = std::async(std::launch::async,
+                                 [&]() { return procTable.getProcesses(); });
+
+    cpuUsage = cpuFuture.get();
+    memUsage = memFuture.get();
+    processes = procFuture.get();
+
+    std::system("clear");
+
     std::cout << "CPU load:\n";
-    cpuUsage = info.getCpuUsage();
+
     std::cout << "Total usage = " << cpuUsage.totalUsage << "%\n";
     for (int i = 0; i < cpuUsage.perCoreUsage.size(); i++) {
       std::cout << "Core " << i << " usage = " << cpuUsage.perCoreUsage[i]
                 << "%\n";
     }
-    memUsage = info.getMemoryUsage();
 
     std::cout << "Total RAM: " << memUsage.totalKB << " kB\n";
     std::cout << "Used RAM: " << memUsage.usedKB << " kB\n";
     std::cout << "Available RAM: " << memUsage.availableKB << " kB\n";
     std::cout << "RAM usage: " << memUsage.usedPercent << "%\n";
 
-    processes = procTable.getProcesses();
     std::cout << "Active processes: " << processes.size() << "\n\n";
-    for (auto x : processes) {
-      std::cout << x << "\n";
+    for (int i = 0; i < processes.size(); i++) {
+      std::cout << processes[i] << "\n";
     }
 
-    break;
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
   }
 
   return 0;
